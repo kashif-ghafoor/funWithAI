8878d1e09f642558779709796380345729a4c3b1
kashif_ghafoor <kashifghafoor140@gmail.com>
2022-05-21 18:07:43 +0500

    fun with AI project completd.

---
+++ pages/api/generate.js
@@ -0,0 +1,19 @@
+import { Configuration, OpenAIApi } from "openai";
+import "dotenv/config";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion("text-curie-001", {
+    prompt: req.body.promptInput,
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+  res.status(200).json({ result: completion.data.choices });
+}
---
+++ pages/index.jsx
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+import { Component } from "react/cjs/react.development";
+
+class Home extends Component {
+  state = {
+    promptInput: "",
+    responses: [],
+  };
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ promptInput: this.state.promptInput }),
+    });
+    const data = await response.json();
+    this.updateResponses(data.result[0].text);
+  };
+  updateResponses = (result) => {
+    const response = {
+      prompt: this.state.promptInput,
+      response: result,
+    };
+    const { responses } = { ...this.state };
+    responses.unshift(response);
+    this.setState({ responses });
+  };
+  handleChange = ({ currentTarget: input }) => {
+    this.setState({ promptInput: input.value });
+  };
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Fun With AI</title>
+        </Head>
+
+        <main className={styles.main}>
+          <h1>Fun With AI</h1>
+          <form onSubmit={this.onSubmit}>
+            <textarea
+              type="textarea"
+              name="prompt"
+              placeholder="Enter Prompt"
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Generate response" />
+            <h2>Responses</h2>
+            {this.state.responses.map((response) => (
+              <div className={styles.result}>
+                <div className={styles.heading}>prompt:</div>
+                <div>{response.prompt}</div>
+                <div className={styles.heading}>response:</div>
+                <div>{response.response}</div>
+              </div>
+            ))}
+          </form>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Home;
